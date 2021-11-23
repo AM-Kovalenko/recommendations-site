311e20912b6044c7eb0df4798ee6d65234e0935c
Alexander <55362298+AM-Kovalenko@users.noreply.github.com>
2021-11-23 17:17:10 +0300

    add allowed hosts

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)+bdyq8gx#)9w@126wj-8@t@dbinoi92_8sxzzx0n8us$0++#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','0.0.0.0','recommendations-site.herokuapp.com']
 
 
 # Application definition
