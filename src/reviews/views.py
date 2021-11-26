fd9aa6261ece20a3ce7af3dd2d9a76487a981912
Alexander <55362298+AM-Kovalenko@users.noreply.github.com>
2021-11-26 03:27:19 +0300

    add views, media, static

--- src/reviews/views.py
+++ src/reviews/views.py
@@ -1,6 +1,36 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
+from .models import *
 
 
-# функция представления главной страницы -> переделываю в класс представления (основанном на базовом классе ListView)
+# функция представления главной страницы
 def index(request):
-    return render(request, 'reviews/index.html')
+    cats = Category.objects.all()
+    posts = Review.objects.all()
+    context = {
+        'cats': cats,
+        'posts': posts,
+    }
+
+    return render(request, 'reviews/index.html', context=context)
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"отображение статьи с id={post_id}")
+
+
+def show_category(request, cat_id):
+    cats = Category.objects.all()
+    posts = Review.objects.filter(cat_id=cat_id)
+
+    if len(posts) == 0:
+       raise Http404()
+
+    context = {
+        'cats': cats,
+        'posts': posts,
+    }
+    return render(request, 'reviews/index.html', context=context)
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
